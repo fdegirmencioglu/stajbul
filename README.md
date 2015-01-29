fac2b38a178c81745a1da352193fef76b891237f
Bertanu <bertan_ulusoy@yahoo.com>
2015-01-29 11:44:20 +0200

    second commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# stajbul
-end project
+# staj-bul
---
+++ app/views/home.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en" ng-app="myApp">
+<head>
+	<meta charset="UTF-8">
+	<title>Stajbul</title>
+</head>
+<body>
+	<div ng-controller="appController as appCtrl">
+		
+		<input ng-model="hello" type="text" />
+		<div>{{ hello }}</div>
+
+	</div>
+	
+
+	<script type="text/javascript" src="js/libs/angular.min.js"></script>
+	<script type="text/javascript" src="js/controller/controller.js"></script>
+</body>
+</html>
