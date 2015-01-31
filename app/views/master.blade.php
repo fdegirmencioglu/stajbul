edbbbc05dadf9711fd3d0d17f882ffb91922f60a
Ferhat DEĞİRMENCİOĞLU <fdegirmencioglu@gmail.com>
2015-01-31 17:26:49 +0200

    Test temizliği

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -5,20 +5,17 @@
     </head>
     <body class="login">
         <header class="row">
-        @include('includes.header')
-    </header>
+            @include('includes.header')
+        </header>
 
     <div class="small-12 small-centered medium-8 medium-centered large-5 large-centered columns">
         @yield('content')
     </div>
 
-    EVDEN TEST
-    
-    
     <footer class="row">
         @include('includes.footer')
     </footer>
-   
-@include('includes.foot')
+
+    @include('includes.foot')
 </body>
 </html>
