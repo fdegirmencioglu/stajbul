b04dd90d6fa60077b936a0c18353ae343b225b42
Bertanu <bertan_ulusoy@yahoo.com>
2015-02-24 20:14:05 +0200

    AngularJS tekrar aktif hale getirildi.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" ng-app="Stajbul">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -21,8 +21,8 @@
 </head>
 <body>
     <header>
-
-
+      <base href="/">
+ 
         <nav class="top-bar foundation-bar docs-bar" data-topbar role="navigation">
             <ul class="title-area">
                 <li class="name">
@@ -182,52 +182,22 @@
 
         </ul> 
     </div>
-</div> <!-- End #sidebar -->
-<!--<nav class="navbar navbar-default">
-        <div class="container-fluid">
-                <div class="navbar-header">
-                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                                <span class="sr-only">Toggle Navigation</span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                                <span class="icon-bar"></span>
-                        </button>
-                        <a class="navbar-brand" href="#">Laravel</a>
-                </div>
+</div> 
+<!-- End #sidebar -->
 
-                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                        <ul class="nav navbar-nav">
-                                <li><a href="/">Home</a></li>
-                        </ul>
-
-                        <ul class="nav navbar-nav navbar-right">
-                                @if (Auth::guest())
-                                        <li><a href="/auth/login">Login</a></li>
-                                        <li><a href="/auth/register">Register</a></li>
-                                @else
-                                        <li class="dropdown">
-                                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
-                                                <ul class="dropdown-menu" role="menu">
-                                                        <li><a href="/auth/logout">Logout</a></li>
-                                                </ul>
-                                        </li>
-                                @endif
-                        </ul>
-                </div>
-        </div>
-</nav>-->
-<div class="page-content">
-
+<div class="page-content"> 
     @yield('content')
 </div>
 
+ 
 <!-- Scripts -->
 <script type="text/javascript" src="/js/vendor/jquery.js"></script>
-<script type="text/javascript" src="/js/vendor/angular.min.js"></script>
-<script type="text/javascript" src="/js/foundation.min.js"></script>
-
+<script type="text/javascript" src="/js/vendor/angular.min.js"></script> 
+<script type="text/javascript" src="/js/vendor/angular-route.min.js"></script> 
+<script type="text/javascript" src="/js/foundation.min.js"></script> 
 <script type="text/javascript" src="/js/app.js"></script>
-<script type="text/javascript" src="/js/controller/LoginController.js"></script>
+<script type="text/javascript" src="/js/services/ProfilesFactory.js"></script>
+<script type="text/javascript" src="/js/controller/ProfilesController.js"></script>
 
 <script>
     $(document).foundation();
